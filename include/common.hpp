86eff053ee203e40f2898ab0e301b885cbbbe265
Mahestro320 <142752255+Mahestro320@users.noreply.github.com>
2025-07-05 14:22:36 +0200

    Add files via upload

--- include/common.hpp
+++ include/common.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <string>
-
-#define DEBUG
+#include "util/version.hpp"
 
 namespace common {
 
 inline std::string main_thread_id{};
 inline bool is_running{true};
+
+inline constexpr Version VERSION{0, 1, 0};
 
 } // namespace common
