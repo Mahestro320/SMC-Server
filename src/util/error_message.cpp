4d7afbd8462466e597890695cea38608fe5bcc64
Mahestro320 <142752255+Mahestro320@users.noreply.github.com>
2025-10-27 16:23:57 +0100

    Delete src directory

--- src/util/error_message.cpp
+++
@@ -1,9 +0,0 @@
-#include "util/error_message.hpp"
-
-std::string util::error_message::get(int errnum) {
-    char buffer[0xFF];
-    if (strerror_s(buffer, sizeof(buffer), errnum) == 0) {
-        return std::string{buffer};
-    }
-    return "Unknown error";
-}
--- src/util/fs.cpp
+++
@@ -1,8 +0,0 @@
-#include "util/fs.hpp"
-
-namespace stdfs = std::filesystem;
-
-bool util::fs::haveDriveLetter(const stdfs::path& path) {
-    const std::string str_path{path.string()};
-    return str_path.size() >= 2 ? str_path[1] == ':' && std::isalpha(str_path[0]) : false;
-}
